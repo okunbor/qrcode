730653c62f980577275541177b36924e2df61a83
okunbor <109839924+okunbor@users.noreply.github.com>
2024-04-06 19:45:36 +0100

    Update qrscanner.js
    updated

--- js/qrscanner.js
+++ js/qrscanner.js
@@ -93,16 +93,16 @@ Html5Qrcode.getCameras().then(devices => {
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
     
     // If you want to prefer front camera
-    html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+   // html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
     
     // If you want to prefer back camera
-    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+   // html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
     
     // Select front camera or fail with `OverconstrainedError`.
     // html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
     
     // Select back camera or fail with `OverconstrainedError`.
-    // html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
+     html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
 
 
   }
